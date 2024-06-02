3aa2a1556291798df2f845be1bb084307704bbfb
DevxMike <michalbaz@poczta.onet.pl>
2024-06-02 11:00:59 -0400

    modified scripts

--- scripts/modbus/modbus_client.py
+++ scripts/modbus/modbus_client.py
@@ -1,10 +1,34 @@
-from pymodbus.client.sync import ModbusTcpClient
+import asyncio
+import logging
+import sys
+import pymodbus.client as modbus_async_client
+from pymodbus.exceptions import ModbusException
 
-client = ModbusTcpClient('host_IP', port=5020)  
-client.connect()
+async def run_modbus_client():
+    client = modbus_async_client.ModbusTcpClient('127.0.0.1', port=5020)
 
-result = client.read_holding_registers(0, 10)
-print(result.registers)
+    try:
+        client.connect()
 
-client.close()
+        rr_coils = client.read_coils(0, 10)  
+        rr_registers = client.read_holding_registers(0, 10) 
+        
+        print("Odczytane I/O: ", rr_coils.bits)
+        print("Odczytane rejestry przed ustawieniem: ", rr_registers.registers)
 
+        write_registers = [i for i in range(10)]
+        client.write_registers(0, write_registers)
+        rr_registers_after = client.read_holding_registers(0, 10)
+        print("Odczytane rejestry po ustawieniu: ", rr_registers_after.registers)
+
+    except ModbusException as e:
+        print("Błąd Modbus:", e)
+
+    finally:
+        client.close()
+
+async def main():
+    await run_modbus_client()
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- scripts/modbus/modbus_server.py
+++ scripts/modbus/modbus_server.py
@@ -1,10 +1,31 @@
-from pymodbus.server.sync import StartTcpServer
-from pymodbus.datastore import ModbusSequentialDataBlock
-from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
+import asyncio
+import logging
+import sys
 
-block = ModbusSequentialDataBlock(0x00, [0]*100)
+from pymodbus import __version__ as pymodbus_version
+from pymodbus.datastore import (
+    ModbusSequentialDataBlock,
+    ModbusServerContext,
+    ModbusSlaveContext,
+    ModbusSparseDataBlock,
+)
+from pymodbus.device import ModbusDeviceIdentification
+from pymodbus.server import (
+    StartAsyncSerialServer,
+    StartAsyncTcpServer,
+    StartAsyncTlsServer,
+    StartAsyncUdpServer,
+)
+
+# Konfiguracja pamięci Modbus z przykładowymi danymi
+block = ModbusSequentialDataBlock(0x00, [0] * 100)
 store = ModbusSlaveContext(di=block, co=block, hr=block, ir=block)
 context = ModbusServerContext(slaves=store, single=True)
 
-StartTcpServer(context, address=("0.0.0.0", 5020))
+# Uruchomienie serwera Modbus TCP/IP
+async def run_server():
+    server = await StartAsyncTcpServer(context, address=("0.0.0.0", 5020))
+    await server.serve_forever()
+
+asyncio.run(run_server())
 
